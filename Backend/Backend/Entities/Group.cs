f08abcc22836b6d2160aa2f3266b9730a28612e0
sanidhyapandey <pandeysanid@gmail.com>
2024-11-20 06:48:05 +0000

    DataBase Connection Established

--- Backend/Backend/Entities/Group.cs
+++ Backend/Backend/Entities/Group.cs
@@ -7,14 +7,14 @@ namespace MatCron.Backend.Entities
         public int Id { get; set; } // Primary Key
         public int OrgId { get; set; } // Foreign Key to Organisation
         public int UserId { get; set; } // Foreign Key to User
-        public string? ContactNumber { get; set; }
-        public string? Status { get; set; }
+        //public string? ContactNumber { get; set; }
+        public byte? Status { get; set; }
 
         // Navigation Properties
         public Organisation? Organisation { get; set; }
-        public User? User { get; set; }
+        public ICollection<User> Users { get; set; }
         public ICollection<Mattress>? Mattresses { get; set; }
         public ICollection<MattressGroup>? MattressGroups { get; set; }
-
+       
     }
 }
