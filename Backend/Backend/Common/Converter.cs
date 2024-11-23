1ba82e746ab625f2d042b32b3b0709d33db1ad41
sanidhyapandey <pandeysanid@gmail.com>
2024-11-23 08:19:19 +0000

    Initialized Converter.cs to convert DTO to Entity

---
+++ Backend/Backend/Common/Converter.cs
@@ -0,0 +1,14 @@
+using MatCron.Backend.DTOs;
+using MatCron.Backend.Entities;
+
+namespace MatCron.Backend.Common
+{
+    public static class Converter
+    {
+
+        public static void ConvertToUser(RegistrationRequestDto dto, int organisationId)
+        {
+
+        }
+    }
+}
