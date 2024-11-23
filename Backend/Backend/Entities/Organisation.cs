84d72b4f853e0d95d2cebc5b341d133b39fe53df
sanidhyapandey <pandeysanid@gmail.com>
2024-11-23 09:28:20 +0000

    Changed the DB changes to use GUID for ID

--- Backend/Backend/Entities/Organisation.cs
+++ Backend/Backend/Entities/Organisation.cs
@@ -1,22 +1,20 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Collections.Generic;
 
 namespace MatCron.Backend.Entities
 {
     public class Organisation
     {
-        public int Id { get; set; } // Primary Key
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? Description { get; set; }
-        public string? PostalAddress { get; set; }
-        public string? NormalAddress { get; set; }
-        public string? WebsiteLink { get; set; }
-        public string? Logo { get; set; }
-        public string? RegistrationNo { get; set; }
-        public string? OrganisationType { get; set; }
-        public string? OrganisationCode { get; set; }
-
-        
+        public Guid Id { get; set; } 
+        public string? Name { get; set; } 
+        public string? Email { get; set; } 
+        public string? Description { get; set; } 
+        public string? PostalAddress { get; set; } 
+        public string? NormalAddress { get; set; } 
+        public string? WebsiteLink { get; set; } 
+        public string? Logo { get; set; } 
+        public string? RegistrationNo { get; set; } 
+        public string? OrganisationType { get; set; } 
+        public string OrganisationCode { get; set; } 
 
         // Navigation Property
         public ICollection<User> Users { get; set; } // Organisation has many Users
