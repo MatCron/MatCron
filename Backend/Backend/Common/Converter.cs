51683825854059662b41588b73dbaddeb69f503a
sanidhyapandey <pandeysanid@gmail.com>
2024-11-23 08:34:39 +0000

    SetUp Converter for OOP and readability purpose

--- Backend/Backend/Common/Converter.cs
+++ Backend/Backend/Common/Converter.cs
@@ -1,3 +1,4 @@
+using Backend.Common.Enums;
 using MatCron.Backend.DTOs;
 using MatCron.Backend.Entities;
 
@@ -6,9 +7,23 @@ namespace MatCron.Backend.Common
     public static class Converter
     {
 
-        public static void ConvertToUser(RegistrationRequestDto dto, int organisationId)
+        public static User ConvertToUser(RegistrationRequestDto dto, int organisationId)
         {
+            if (dto == null)
+            {
+                return null;
+            }
 
+            return new User
+            {   Id = Guid.NewGuid(),
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                Email = dto.Email,
+                Password = dto.Password, 
+                OrgId = organisationId,
+                UserType = (byte)dto.UserType,
+                EmailVerified = (byte)EmailStatus.Pending
+            };
         }
     }
 }
