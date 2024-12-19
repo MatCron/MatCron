76f2f2271932d52a560e8361fa0a0394e808f270
sanidhyapandey <pandeysanid@gmail.com>
2024-12-19 22:43:40 +0000

    Added Entity for Mattress, Logs and Location

--- Backend/Backend/Entities/Mattress.cs
+++ Backend/Backend/Entities/Mattress.cs
@@ -1,22 +1,29 @@
-﻿namespace MatCron.Backend.Entities
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatCron.Backend.Entities
 {
     public class Mattress
     {
-        public Guid Uid { get; set; } // Primary Key
-        public Guid MattressTypeId { get; set; } // Foreign Key to MattressType
-        public string? BatchNo { get; set; }
-        public DateTime ProductionDate { get; set; }
-        public Guid GroupId { get; set; } // Foreign Key to Group
-        public Guid? UserId { get; set; } // Foreign Key to User
-        public string? EpcCode { get; set; }
-        public byte Status { get; set; }
-        public DateTime? LifeCyclesEnd { get; set; }
-        public int DaysToRotate { get; set; }
+        public Guid Uid { get; set; }  
+        public Guid MattressTypeId { get; set; }  
+        public Guid? OrganisationId { get; set; }  
+        public Guid? LocationId { get; set; }  
+
+        public string? BatchNo { get; set; }  
+        public DateTime ProductionDate { get; set; }  
+        public string? EpcCode { get; set; }  
+        public byte Status { get; set; }  
+        public DateTime? LifeCyclesEnd { get; set; }  
+        public int DaysToRotate { get; set; }  
 
         // Navigation Properties
         public MattressType MattressType { get; set; }
-        public Group? Group { get; set; }
-        public User? User { get; set; }
-        public ICollection<Log>? Logs { get; set; } = new List<Log>();
+        public Organisation? Organisation { get; set; }
+        public LocationMattress? Location { get; set; }
+        public ICollection<LogMattress> Logs { get; set; } = new List<LogMattress>();
+        public ICollection<MattressGroup> MattressGroups { get; set; } = new List<MattressGroup>();
+
+ 
     }
 }
