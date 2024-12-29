25d489af4d5d47e660ee59350a178374997e7c2c
sanidhyapandey <pandeysanid@gmail.com>
2024-12-29 03:40:22 +0000

    Created DTO for Editing Mattresses in a group and complete GroupDTO

---
+++ Backend/Backend/DTOs/Group/GroupDto.cs
@@ -0,0 +1,14 @@
+using Backend.Common.Enums;
+
+namespace MatCron.Backend.DTOs
+{
+    public class GroupDto
+    {
+        public Guid Id { get; set; } // Unique identifier for the group
+        public string Name { get; set; } // Name of the group
+        public string? Description { get; set; } // Optional description
+        public DateTime CreatedDate { get; set; } // Date the group was created
+        public GroupStatus Status { get; set; } // Status (Active/Archived)
+        public int MattressCount { get; set; } // Count of mattresses in this group
+    }
+}
