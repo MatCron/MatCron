004b28ae0f027a5722e3da1868539a0115e1110e
sanidhyapandey <pandeysanid@gmail.com>
2024-12-29 03:36:12 +0000

    Create DTO for Creating a new Group

---
+++ Backend/Backend/DTOs/Group/GroupCreateDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MatCron.Backend.DTOs
+{
+    public class GroupCreateDto
+    {
+        [Required(ErrorMessage = "The name field is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The description field is required.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "The orgId field is required.")]
+        public Guid OrgId { get; set; }
+    }
+}
