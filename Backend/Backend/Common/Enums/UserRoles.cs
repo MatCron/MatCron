807387af953e7106c0470a7aa5971a877e4d3348
Lee Xuan Ong <ongtan597@gmail.com>
2025-01-20 22:02:00 +0000

    user roles implement

---
+++ Backend/Backend/Common/Enums/UserRoles.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Common.Enums
+{
+    public enum UserRoles
+    {
+        SuperAdmin = 4,
+        Manufacturer = 3,
+        Manager = 2,
+        User = 1,
+        Guest =0
+    }
+}
