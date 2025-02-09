39b419b90025854695b8f9092c64ecac75a84e39
sanidhyapandey <pandeysanid@gmail.com>
2025-02-09 04:53:30 +0000

    Created Snackbar Widget to manage validation messages

---
+++ web-app/src/components/Snackbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Snackbar, Alert } from "@mui/material";
+
+const CustomSnackbar = ({ open, handleClose, message, severity }) => {
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={3000} // Automatically hides after 3 seconds
+      onClose={handleClose}
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+    >
+      <Alert onClose={handleClose} severity={severity} sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default CustomSnackbar;
