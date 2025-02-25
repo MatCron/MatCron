5f0d5fb18799f4472cc98546c743a89eadde0880
sanidhyapandey <pandeysanid@gmail.com>
2025-02-25 03:03:30 +0000

    Added Line Chart for comparing Mattress Statuses

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -5,11 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme } from '@mui/material';
-
-const theme = createTheme();
+import theme from './theme'; // Import the custom theme
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
